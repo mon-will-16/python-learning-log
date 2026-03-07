22e11dac1d5cbd0c908c1510c72e5d787e4c7e5f
Monica Williamson <techflow16@yahoo.com>
2026-03-06 21:02:20 -0500

    Get User Input Lesson

---
+++ s8_62_getting_user_input.py
@@ -0,0 +1,12 @@
+# Section 8 | Lesson 62 | Boolean and Conditional Logic
+# Course: Modern Python 3 Bootcamp - Colt Steele
+# Topic: Getting User Input
+
+# --- Practice Code ---
+
+data = input("what's your favorite color?")
+print("You said " + data)   
+
+print("What's your favorite color?") 
+data = input()
+print("You said " + data)
