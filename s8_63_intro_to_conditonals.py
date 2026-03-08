ff377f55e89731ca20b0cd0c55b49ac5154ccd88
Monica Williamson <techflow16@yahoo.com>
2026-03-07 19:10:03 -0500

    Intro to Conditioinals

---
+++ s8_63_intro_to_conditonals.py
@@ -0,0 +1,17 @@
+# Section 8 | Lesson 63 | Intro to Conditionals
+# Course: Modern Python 3 Bootcamp - Colt Steele
+# Topic: Intro to Conditionals
+
+# --- Practice Code ---
+
+name = "Jon Snow"
+if name == "Arya Stark":
+    print("Valar Morghulis")
+elif name == "Jon Snow":
+    print("You know nothing")
+else:
+    print("Carry on")
+
+
+
+
