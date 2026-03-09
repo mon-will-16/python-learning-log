3ea5767b2c1eded09bd4e7e6db0abb3fd0fa9816
Monica Williamson <techflow16@yahoo.com>
2026-03-09 16:01:29 -0400

    Lesson on Truthiness

---
+++ s8_67_word_on_truthiness.py
@@ -0,0 +1,12 @@
+# Section 8 | Lesson 67 | A Word on Truthiness
+# Course: Modern Python 3 Bootcamp - Colt Steele
+# Topic: A Word on Truthiness
+
+# --- Practice Code ---
+animal = input("enter your favorite animal")
+
+if animal:
+    print(animal + " is my favorite animal too!")
+else: 
+    print("You didn't say anything!")
+
