4312e84a0867a6233676fb8bd0dc3b9f56f2b960
Monica Williamson <techflow16@yahoo.com>
2026-03-14 19:41:01 -0400

    Refactoring

---
+++ module_2_control_flow_and_logic/s9_78_rps_mini_project_refactoring_time.py
@@ -0,0 +1,38 @@
+# Section 9 | Lesson 77 | RPS Mini Project Basic Version Solution
+# Course: Modern Python 3 Bootcamp - Colt Steele
+# Topic:    RPS Mini Project Basic Version Solution
+
+# --- Practice Code ---
+print("Rock...")
+print("Paper...")
+print("Scissors...")
+
+player1 = input("player 1, make yor move: ")
+player2 = input("player 2, make your move: ")
+
+if player1 == player2:   
+    print("It's a tie!")
+elif player1 == "rock":
+    if player2 == "scissors":
+        print("player 1 wins!")
+    elif player2 == "paper":
+        print("player2 wins!")
+elif player1 == "paper":
+    if player2 == "rock":
+        print("player 1 wins!")
+    elif player2 == "scissors":
+        print("player 2 wins!")
+elif player1 == "scissors":
+    if player2 == "paper": 
+        print("player 1 wins!")
+    elif player2 == "rock":
+        print("player 2 wins!")
+else:
+    print("something went wrong") 
+
+
+
+
+
+
+
