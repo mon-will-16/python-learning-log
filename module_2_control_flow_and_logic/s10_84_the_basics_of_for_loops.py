3c491a1911e346af59a753189481a8243d43a326
Monica Williamson <techflow16@yahoo.com>
2026-03-19 19:02:22 -0400

    For Loops

---
+++ module_2_control_flow_and_logic/s10_84_the_basics_of_for_loops.py
@@ -0,0 +1,12 @@
+# Section 10 | Lesson 84 | The Basics of For Loops
+# # Course: Modern Python 3 Bootcamp - Colt Steele
+# Topic:    The Basics of For Loops
+
+#--- Practice Code ---
+
+# for x in range(1,10):
+#     print(x)
+#     print(x*x)
+
+for letter in "coffee":
+    print(letter*10)
---
+++ s10_85_exploring_ranges_in_depth.py
@@ -0,0 +1,12 @@
+# Section 10 | Lesson 85 | Exploring Ranges in Depth
+# # Course: Modern Python 3 Bootcamp - Colt Steele
+# Topic:    Exploring Ranges in Depth
+
+#--- Practice Code ---
+# Ranges are immutable sequences of numbers commonly used for looping a specific number of times in for loops.
+
+# for num in range(10):
+#     print(num)  
+
+for num in range(10, 20, 2):
+    print(num)  
