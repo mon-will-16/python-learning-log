dc33204129723e961bfe7994e0b1883d5ab52336
Monica Williamson <techflow16@yahoo.com>
2026-05-14 16:26:29 -0400

    Iterating over lists

---
+++ s12_103_iterating_over_lists_for_loop.py
@@ -0,0 +1,9 @@
+# Section 12 | Lesson 103 | Iterating Over Lists
+# Course: Modern Python 3 Bootcamp - Colt Steele
+# Topic:    Iterating Over Lists
+
+#--- Practice Code ---
+numbers = [1, 2, 3, 4, True, 8.9, "hello"]
+
+for number in numbers:
+    print(number)
---
+++ s12_103_iterating_over_lists_while_loop.py
@@ -0,0 +1,10 @@
+# Section 12 | Lesson 103 | Iterating Over Lists
+# Course: Modern Python 3 Bootcamp - Colt Steele
+# Topic:    Iterating Over Lists
+
+#--- Practice Code ---
+numbers = [1, 2, 3, 4]
+i = 0
+while i < len(numbers):
+    print(numbers[i])
+    i += 1
